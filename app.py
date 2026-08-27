27a2c4510bce1503b6b210306c8f8669d043af58
heitorandrd <capir.2025116isinf0011@aluno.ifpi.edu.br>
2026-08-27 11:15:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def home():
+    return "Olá, mundo! Projeto Flask - Dupla Heitor e Kavanny"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
